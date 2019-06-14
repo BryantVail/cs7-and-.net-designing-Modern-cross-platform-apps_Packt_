e0bb161b3dd8875b47253e221a1e3bd396f14d3c
Bryant Vail <Bryanttv@outlook.com>
2019-06-14 08:51:08 -0400

    update

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace cs7_and_.net_designing_Modern_cross_platform_apps_Packt_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            if(args.Length == 0){
+                WriteLine("There are no arguments");
+            }
+            else{
+                WriteLine("There is at least One Argument");
+            }
+        }
+    }
+}
